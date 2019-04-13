dc6ef61ac43e0ccade866cbdfb0923bab1d7442d
tdolega <tymoteuszdolega@gmail.com>
2019-04-13 21:11:14 +0200

    polowa 2016

--- 2017/4.1.py
+++ 2017/4.1.py
@@ -4,7 +4,7 @@ i = 0
 najdluzszy = ""
 for x in c:
     x = x[:-1]
-    x1, x2 = x[:int(len(x)/2)], x[int(len(x)/2):]
+    x1, x2 = x[:int(len(x) / 2)], x[int(len(x) / 2):]
     if x1 == x2:
         i += 1
         if len(x) > len(najdluzszy):
--- 2017/4.2.py
+++ 2017/4.2.py
@@ -4,7 +4,7 @@ bledne = 0
 najmniejsze = 99999  # xd
 for x in c:
     x = x[:-1]
-    bcd = [x[i:i+4] for i in range(0, len(x), 4)]
+    bcd = [x[i:i + 4] for i in range(0, len(x), 4)]
     for y in bcd:
         if int(y, 2) > 9:
             bledne += 1
