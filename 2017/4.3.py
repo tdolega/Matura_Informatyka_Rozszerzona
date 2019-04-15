552f19052af7ce5949afa96a48998db8b73563d2
tdolega <tymoteuszdolega@gmail.com>
2019-04-15 14:10:18 +0200

    usuwanie starych smieci

--- 2017/4.3.py
+++
@@ -1,9 +0,0 @@
-f = open("Dane_PR2/binarne.txt", "r")
-c = f.readlines()
-najwieksza = 0
-for x in c:
-    dx = int(x, 2)
-    if dx < 65535:
-        if dx > najwieksza:
-            najwieksza = dx
-print(najwieksza, bin(najwieksza)[2:])
--- 2018/6.2.py
+++
@@ -1,8 +0,0 @@
-f = open("Dane_PR2/slowa.txt", "r")
-c = f.readlines()
-i = 0
-for x in c:
-    x1, x2 = x.split(" ")
-    if x1 in x2:
-        i += 1
-print(i)
